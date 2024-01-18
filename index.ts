0b3d4118dc49d1412eedb4bc718201a4c8279d45
Coach Chuck <christian.krueger.health@gmail.com>
2024-01-17 17:02:08 -0700

    working

---
+++ index.ts
@@ -0,0 +1,60 @@
+import {Client, Events, GatewayIntentBits, Partials} from 'discord.js';
+import * as api from 'clicksend';
+
+
+// --------------- CLICKSEND BOT -----------------
+
+const CLICKSEND_USERNAME = process.env.CLICKSEND_USERNAME as string;
+const CLICKSEND_API_KEY = process.env.CLICKSEND_API_KEY as string;
+const CLICKSEND_FROM = "myNumber";
+const CLICKSEND_TO = process.env.CLICKSEND_TO as string;
+
+const sms = new api.SMSApi(CLICKSEND_USERNAME, CLICKSEND_API_KEY);
+
+function sendSMS(message: string) {
+
+    const smsMessage = new api.SmsMessage();
+    smsMessage.from = CLICKSEND_FROM;
+    smsMessage.to = CLICKSEND_TO;
+    smsMessage.body = message;
+
+    const smsCollection = new api.SmsMessageCollection();
+    smsCollection.messages = [smsMessage];
+
+    sms.smsSendPost(smsCollection).then(function(response: any) {
+        console.log("Message sent!")
+    }).catch(function(err: any){
+        console.error(`err.code: ${err}`);
+    });
+}
+
+// --------------- DISCORD BOT -----------------
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
+
+// create a new Client instance
+const client = new Client({
+    intents: [GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
+    partials: [Partials.Channel, Partials.Message, Partials.Reaction],
+});
+
+// listen for the client to be ready
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+    if(message.channel.id === DISCORD_CHANNEL_ID) {
+        console.log(message.content);
+        sendSMS(message.content);
+    }
+ })
+
+ client.on("error", (error) => {
+    console.log(error);
+})
+
+
+// --------------- APP -----------------
+client.login(DISCORD_TOKEN);
